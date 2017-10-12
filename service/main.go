8ffb2c70c0b190c5c809b909f757e2b1e656da1f
Ewan Valentine <ewan.valentine@bbc.co.uk>
2017-10-12 07:59:59 +0100

    init

---
+++ service/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/TabbDrinkLTD/envtest/service/models"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Authenticate(ctx context.Context, in *pb.AuthRequest) (*pb.AuthResponse, error) {
+	return &pb.AuthResponse{Done: true}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterAuthServiceServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
